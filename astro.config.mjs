a00c70891c0ae103ab93b128fa594a2d6bba6c6a
GetUINUX <founder@getuinux.com>
2026-02-07 23:03:19 +0000

    UINUX BLOG-v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  site: "https://blog.uinux.co",
+  site: "https://uinuxblog.getuinux.com",
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
